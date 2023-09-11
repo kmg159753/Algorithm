e3a172e8fdff47d4bf4fe3d57c7bacedf2ac3e30
ANGO <84438772+kmg159753@users.noreply.github.com>
2023-09-12 00:28:47 +0900

    [Silver I] Title: 절댓값 힙, Time: 632 ms, Memory: 27192 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.PriorityQueue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        PriorityQueue<Integer> pq = new PriorityQueue<>((o1, o2) -> {
+            int first_abs = Math.abs(o1);
+            int second_abs = Math.abs(o2);
+
+            if (first_abs == second_abs) {
+                return o1 > o2 ? 1 : -1;
+            }
+            return first_abs - second_abs;
+        });
+
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num != 0) {
+                pq.add(num);
+
+            } else {
+                if (pq.isEmpty()) {
+                    System.out.println(0);
+                } else {
+                    System.out.println(pq.poll());
+                }
+            }
+        }
+    }
+}
